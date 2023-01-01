bd330128a580b7204c8bf86beed5c3e05e51acbe
riotrequest <121694664+riotrequest@users.noreply.github.com>
2023-01-01 17:15:03 -0500

    Update addsite.sh

--- addsite.sh
+++ addsite.sh
@@ -3,6 +3,9 @@
 # get the domain name from the first argument
 domain=$1
 
+# generate a random password for the MySQL user
+password=$(pwgen -s 12 1)
+
 # create a new directory for the website
 mkdir -p /var/www/$domain/public_html
 
@@ -41,3 +44,36 @@ EOF
 
 # restart nginx to apply the changes
 systemctl restart nginx
+
+# create a new MySQL database and user for the website
+mysql -u root -p <<EOF
+CREATE DATABASE $domain;
+GRANT ALL PRIVILEGES ON $domain.* TO '$domain'@'localhost' IDENTIFIED BY '$password';
+FLUSH PRIVILEGES;
+EOF
+
+# download and extract WordPress
+wget -q https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+rm latest.tar.gz
+mv wordpress/* /var/www/$domain/public_html
+
+# copy the sample configuration file and modify it with the new database information
+cp /var/www/$domain/public_html/wp-config-sample.php /var/www/$domain/public_html/wp-config.php
+sed -i "s/database_name_here/$domain/g" /var/www/$domain/public_html/wp-config.php
+sed -i "s/username_here/$domain/g" /var/www/$domain/public_html/wp-config.php
+sed -i "s/password_here/$password/g" /var/www/$domain/public_html/wp-config.php
+
+# change the ownership of the website directory to the Apache user
+chown -R www-data:www-data /var/www/$domain/public_html
+
+# create a new .htaccess file to block access to the WordPress config file
+cat > /var/www/$domain/public_html/.htaccess <<EOF
+<Files wp-config.php>
+order allow,deny
+deny from all
+</Files>
+EOF
+
+# print the MySQL password to the console
+echo "MySQL password: $password"
